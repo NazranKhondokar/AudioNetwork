3ddeca621962b63a251bb59900b5143df589d9c5
Nazran Khondokar <nazran@rokomari.com>
2019-07-17 11:57:07 +0600

    Initial commit

---
+++ app/src/main/java/com/asraful/audionetwork/PlaybackStatus.java
@@ -0,0 +1,9 @@
+package com.asraful.audionetwork;
+
+/**
+ * Created by Valdio Veliu on 16-07-29.
+ */
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/com/asraful/audionetwork/ViewHolder.java
@@ -0,0 +1,19 @@
+package com.asraful.audionetwork;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView title;
+    ImageView play_pause;
+
+    ViewHolder(View itemView) {
+        super(itemView);
+        title = (TextView) itemView.findViewById(R.id.title);
+        play_pause = (ImageView) itemView.findViewById(R.id.play_pause);
+    }
+}
